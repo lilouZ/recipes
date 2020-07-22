c55cbc4618840d134b4814dd6ab696f96ddf71e0
unknown <5alilzenasni@gmail.com>
2020-07-22 21:23:49 +0100

    add views, urls and prepare templates folder

--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+## home page view
+def home(request):
+    pass
+
+
+
+## all recipes page view
+def recipes(request):
+    recipes = Recipe.objects.all()
+    context = {
+        'recipes':recipes,
+
+    }
+    return render(request, 'main/recipes.html', context)
+
+
+## repies details page view
+def details(request, pk):
+    details = Recipe.objects.get(id=pk)
+    context = {
+        'details':details,
+
+    }
+    return render(request, 'main/details.html', context)
--- prj/urls.py
+++ prj/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipes/', include('main.urls', namespace='main'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
